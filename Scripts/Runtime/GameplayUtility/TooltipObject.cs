fd504f561d2f7d62c846677017971855e716d4ad
Daniel Onstott <50355544+Irotpigs@users.noreply.github.com>
2023-07-07 18:02:04 -0700

    Update tooltips for setting text and disabling entirely

--- Scripts/Runtime/GameplayUtility/TooltipObject.cs
+++ Scripts/Runtime/GameplayUtility/TooltipObject.cs
@@ -17,7 +17,10 @@ namespace Lemongrass
     
     [Tooltip("The type of prefab that should be used with this tooltip")]
     public Subsystem_Tooltips.TooltipType tooltipType = Subsystem_Tooltips.TooltipType.Light;
-    
+
+    [Tooltip("Determines if the tool tip will display or not.")]
+    public bool Visible = true;
+
     //-private-------------------------------------------------------------------//
     [SerializeField, TextArea, Tooltip("This text will be displayed on the tooltip")]
     private string tooltip = "Not Set";
@@ -34,10 +37,19 @@ namespace Lemongrass
       return tooltip;
     }
 
+    /**
+    * @brief Sets the text of this tooltip.
+    * @param newText The new text the tooltip should display.
+    */
+    public void SetText(string newText)
+    {
+      tooltip = newText;
+    }
+
     ///////////////////////////////Pointer Interface///////////////////////////////
     public void OnPointerEnter(PointerEventData eventData)
     {
-      GameInstance.GetSubsystem<Subsystem_Tooltips>().AddTooltip(this);
+      if (Visible) GameInstance.GetSubsystem<Subsystem_Tooltips>().AddTooltip(this);
     }
 
     public void OnPointerExit(PointerEventData eventData)
